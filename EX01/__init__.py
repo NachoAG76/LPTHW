e7ce7234418dbbd9067fcb300650b288a9b2b39e
Nacho <igna.algr@gmail.com>
2018-02-19 09:13:52 +0100

    Ejercitando

---
+++ EX01/__init__.py
@@ -0,0 +1,7 @@
+'''
+  Created by Ignacio Álvarez García 
+  on 19/02/2018 8:20
+ '''
+
+if __name__ == '__main__':
+    pass
---
+++ EX02/ex02.py
@@ -0,0 +1,7 @@
+"""Created by Ignacio Álvarez García
+  on 19/02/2018 9:04
+  """
+
+# Un comentario
+# Tod después de # es ignorado por python.
+print("Yo podría tener un código como este")
